a7d6fc6fd09b0cd17d677f6882fcb08dde262b63
bibek <bibek_786@rediffmail.com>
2020-06-08 07:45:39 +0530

    added print

---
+++ basic_variables_and_datetypes/print.py
@@ -0,0 +1,8 @@
+x==3
+y=5.7
+my_name="bibekanand"
+my_lang="python"
+print("{} \n{} \n{}".format(x,y,my_lang))
+print("{} {} {}".format(x,y,my_lang))
+print("{} {} {}".format(my_lang,y,x))
+print(f"x value is : {x} \n y value is : {y}")
