03d42785e651828657d3c50c8e239a2d4907baca
bibek <bibek_786@rediffmail.com>
2020-06-08 05:56:56 +0530

    added another line

--- python1.py
+++ python1.py
@@ -3,3 +3,4 @@ print ("This is about indetation")
 if 3 > 1:
     print ("this is the comparision of 3 and 1 using if condition")
 print ("We are ok with indentations")
+print ("We are ok with indentations")
