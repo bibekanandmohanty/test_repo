33947fed814eaeb4b1239877aa01020d94347eeb
bibek <bibek_786@rediffmail.com>
2020-06-08 06:48:39 +0530

    addded variable file

---
+++ basic_variables_and_datetypes/variables.py
@@ -0,0 +1,6 @@
+# A variable is nothing but a reserved memory location to store values. In other words a variable in a program giives data to the computer to work on.
+# No need to declare varables before using them.
+# That is why it is called dynamically typed program.
+a=10
+print(a)
+# Dont use quotes around the var name otherwise it will print as it is.
