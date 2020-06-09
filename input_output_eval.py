71c3e4a1916d7d0f89a919f8d8434ef94c240342
bibek <bibek_786@rediffmail.com>
2020-06-10 03:55:34 +0530

    no comments

--- input_output_eval.py
+++ input_output_eval.py
@@ -1,5 +1,6 @@
 a=eval(input("Enter a value:"))
-a=eval(input("Enter b value:"))
+b=eval(input("Enter b value:"))
 result=a+b
 print(f"The addition of {a} and {b} is : {result}")
 print(f"The value of {a} is : {type(a)}")
+print(f"The value of {b} is : {type(b)}")
