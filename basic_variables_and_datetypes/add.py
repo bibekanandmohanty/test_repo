aeea3e8d8918be9a657d8542adb1fdfb1c7669c5
bibek <bibek_786@rediffmail.com>
2020-06-10 04:03:22 +0530

    no comments

--- basic_variables_and_datetypes/add.py
+++ basic_variables_and_datetypes/add.py
@@ -1,5 +1,5 @@
 #Simple addition calculator
-a=eval(input("Enter the value of a: ")
-b=eval(input("Enter the value of b: ")
+a=eval(input("Enter the value of a: "))
+b=eval(input("Enter the value of b: "))
 add=a+b
 print(f"The addition of a and b is: {add}")
