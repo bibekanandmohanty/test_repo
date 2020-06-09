2f56f4691fae43c6f48dc6d7d113faf799c2fd8f
bibek <bibek_786@rediffmail.com>
2020-06-10 03:26:16 +0530

    no comments

--- python1.py
+++ python1.py
@@ -1,5 +1,5 @@
 
-print ("This is about indetation")
+print ("This is about indentation")
 if 3 > 1:
     print ("this is the comparision of 3 and 1 using if condition")
 print ("We are ok with indentations")
