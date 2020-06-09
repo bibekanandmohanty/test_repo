7737ae8bc16168e99bd8308a1cb6076b44dc15c8
bibek <bibek_786@rediffmail.com>
2020-06-10 04:28:27 +0530

    no comments

---
+++ string/create_string.py
@@ -0,0 +1,28 @@
+#Intro to strings
+my_name="bibek"
+my_newname="bibek1"
+print(f"my name is : {my_name} \nmy new name is : {my_newname}")
+space=""
+space1=" "
+print(f"{bool(space)}")
+print(f"{bool(space1)}")
+
+my_fav_script="python scripting"
+print(my_fav_script)
+print(my_fav_script[0])
+print(my_fav_script[5])
+print(my_fav_script[5])
+print(my_fav_script[-1])
+print(my_fav_script[5])
+print(my_fav_script[0:5]) #prints character from 0 to 4
+print(my_fav_script[0:])  #prints 0 to last
+print(my_fav_script[:5])  #prints character from 0 to 4
+print(my_fav_script[4:11])
+
+#strings are immutable.. That means u cant delete or assign part of the string. 
+#length of the string
+mystringlen=len(my_fav_script)
+print(f"length of the given string is : {len(my_fav_script)}")
+print(my_fav_script[-16:-5])
+
+
