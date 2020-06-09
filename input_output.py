5970108066b92a234495b544834cac8010ca5dc4
bibek <bibek_786@rediffmail.com>
2020-06-10 03:53:51 +0530

    no comments

--- input_output.py
+++ input_output.py
@@ -1,5 +1,6 @@
 a=int(input("Enter a value:"))
-a=int(input("Enter b value:"))
+b=int(input("Enter b value:"))
 result=a+b
 print(f"The addition of {a} and {b} is : {result}")
 print(f"The value of {a} is : {type(a)}")
+print(f"The value of {b} is : {type(b)}")
