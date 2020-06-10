b9203a11265a59b5eb8ca7231f4b4d778b465f1f
bibek <bibek_786@rediffmail.com>
2020-06-10 07:51:52 +0530

    no comments

---
+++ string/case/boolean_result_string.py
@@ -0,0 +1,12 @@
+test1="Python is Great"
+test2="PythonisGreat3333"
+print(test1.startswith("P")
+print(test1.endswith("eat")
+print(test1.islower())
+print(test1.isupper())
+print(test1.istitle())
+print(test1.isspace())
+print(test1.isalpha())
+print(test2.isnumeric())
+
+
---
+++ string/case/case1.py
@@ -0,0 +1,5 @@
+string1="Bibekanadd Mohanty"
+print(string1.lower())
+print(string1.upper())
+
+
