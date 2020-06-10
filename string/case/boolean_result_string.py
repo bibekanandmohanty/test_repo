926e923861364c0065ed4e357074e9a930d92546
bibek <bibek_786@rediffmail.com>
2020-06-10 07:54:12 +0530

    no comments

--- string/case/boolean_result_string.py
+++ string/case/boolean_result_string.py
@@ -1,7 +1,7 @@
 test1="Python is Great"
 test2="PythonisGreat3333"
-print(test1.startswith("P")
-print(test1.endswith("eat")
+print(test1.startswith("P"))
+print(test1.endswith("eat"))
 print(test1.islower())
 print(test1.isupper())
 print(test1.istitle())
