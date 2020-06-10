0773cb6d6212cac451d8a9542d40afffaf6aaeda
bibek <bibek_786@rediffmail.com>
2020-06-09 19:13:03 -0700

    no comments

--- string/case/case_conv.py
+++ string/case/case_conv.py
@@ -1,6 +1,7 @@
-test1="bibekanand mohanty"
+test1="biBEekanand MohAnty"
 print(test1.upper())
 print(test1.lower())
 print(test1.swapcase())
 print(test1.title())
 print(test1.capitalize())
+print(test1.casefold())
